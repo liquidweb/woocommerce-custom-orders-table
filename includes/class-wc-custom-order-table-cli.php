73e65bae8d48a9c15ba8884040d89287df57ce81
Steve Grunwell <sgrunwell@liquidweb.com>
2017-12-22 12:52:06 -0500

    Clean up coding standards in class-wc-custom-order-table-cli.php

--- includes/class-wc-custom-order-table-cli.php
+++ includes/class-wc-custom-order-table-cli.php
@@ -1,167 +1,175 @@
 <?php
-
 /**
  * CLI Tool for migrating order data to/from custom table.
- *
- * @version  1.0.0
- * @category Class
  */
-class WC_Custom_Order_Table_CLI extends WP_CLI_Command
-{
 
-    private $count;
+class WC_Custom_Order_Table_CLI extends WP_CLI_Command {
 
-    /**
-     * Count how many orders have yet to be migrated.
-     *
-     * ## EXAMPLES
-     *
-     *     wp wc-order-table count
-     *
-     */
-    public function count() {
-        global $wpdb;
+	/**
+	 * Count how many orders have yet to be migrated.
+	 *
+	 * ## EXAMPLES
+	 *
+	 *     wp wc-order-table count
+	 *
+	 * @global $wpdb
+	 */
+	public function count() {
+		global $wpdb;
 
-        $order_table = wc_custom_order_table()->get_table_name();
+		$order_table = wc_custom_order_table()->get_table_name();
+		$order_count = $wpdb->get_var( $wpdb->prepare(
+			"SELECT COUNT(1)
+			FROM {$wpdb->posts} p
+			LEFT JOIN {$order_table} o ON p.ID = o.order_id
+			WHERE p.post_type IN ('%s')
+			AND o.order_id IS NULL
+			ORDER BY p.post_date DESC",
+			implode( ',', wc_get_order_types( 'reports' ) )
+		) );
 
-        $order_count = $wpdb->get_var( $wpdb->prepare("
-            SELECT COUNT(1)
-            FROM {$wpdb->posts} p
-            LEFT JOIN {$order_table} o ON p.ID = o.order_id
-            WHERE p.post_type IN ('%s')
-            AND o.order_id IS NULL
-            ORDER BY p.post_date DESC
-        ", implode(',', wc_get_order_types('reports'))));
+		WP_CLI::log( sprintf( __( '%d orders to be migrated.', 'wc-custom-order-table' ), $order_count ) );
 
-        WP_CLI::log( sprintf( __( '%d orders to be migrated.', 'wc-custom-order-table' ), $order_count ) );
+		return $order_count;
+	}
 
-        return $order_count;
-    }
+	/**
+	 * Migrate order data to the custom order table.
+	 *
+	 * ## OPTIONS
+	 *
+	 * [--batch=<batch>]
+	 * : The number of orders to process.
+	 * ---
+	 * default: 1000
+	 * ---
+	 *
+	 * [--page=<page>]
+	 * : The page to start from.
+	 * ---
+	 * default: 1
+	 * ---
+	 *
+	 * ## EXAMPLES
+	 *
+	 *     wp wc-order-table migrate --batch=100 --page=1
+	 *
+	 * @global $wpdb
+	 *
+	 * @param array $args       Positional arguments passed to the command.
+	 * @param array $assoc_args Associative arguments (options) passed to the command.
+	 */
+	public function migrate( $args, $assoc_args ) {
+		global $wpdb;
 
-    /**
-     * Migrate order data to the custom order table.
-     *
-     * ## OPTIONS
-     *
-     * [--batch=<batch>]
-     * : The number of orders to process.
-     * ---
-     * default: 1000
-     * ---
-     *
-     * [--page=<page>]
-     * : The page to start from.
-     * ---
-     * default: 1
-     * ---
-     *
-     * ## EXAMPLES
-     *
-     *     wp wc-order-table migrate --batch=100 --page=1
-     *
-     */
-    public function migrate($args, $assoc_args)
-    {
-        global $wpdb;
+		$orders_batch      = isset( $assoc_args['batch'] ) ? absint( $assoc_args['batch'] ) : 1000;
+		$orders_page       = isset( $assoc_args['page'] ) ? absint( $assoc_args['page'] ) : 1;
+		$order_table       = wc_custom_order_table()->get_table_name();
+		$order_count       = $this->count();
+		$total_pages       = ceil( $order_count / $orders_batch );
+		$progress          = \WP_CLI\Utils\make_progress_bar( 'Order Data Migration', $order_count );
+		$batches_processed = 0;
+		$orders_sql        = $wpdb->prepare(
+			"SELECT ID FROM {$wpdb->posts}
+			WHERE post_type IN ('%s')
+			ORDER BY post_date DESC",
+			implode( ',', wc_get_order_types( 'reports' ) )
+		);
 
-        $orders_batch = isset($assoc_args['batch']) ? absint($assoc_args['batch']) : 1000;
-        $orders_page = isset($assoc_args['page']) ? absint($assoc_args['page']) : 1;
+		// Work through each batch to migrate their orders.
+		for ( $page = $orders_page; $page <= $total_pages; $page++ ) {
+			$offset = ( $page * $orders_batch ) - $orders_batch;
+			$orders = $wpdb->get_col( $wpdb->prepare(
+				$orders_sql . ' LIMIT %d OFFSET %d',
+				$orders_batch,
+				max( $offset, 0 )
+			) );
 
-        $order_table = wc_custom_order_table()->get_table_name();
+			foreach ( $orders as $order ) {
+				// Accessing the order via wc_get_order will automatically migrate the order to the custom table.
+				wc_get_order( $order );
 
-        $order_count = $this->count();
+				$progress->tick();
+			}
 
-        $total_pages = ceil($order_count / $orders_batch);
+			$batches_processed++;
+		}
 
-        $progress = \WP_CLI\Utils\make_progress_bar('Order Data Migration', $order_count);
+		$progress->finish();
 
-        $orders_sql = $wpdb->prepare("
-          SELECT ID FROM {$wpdb->posts}
-          WHERE post_type IN ('%s')
-          ORDER BY post_date DESC
-        ", implode(',', wc_get_order_types('reports')));
-        $batches_processed = 0;
+		WP_CLI::log( sprintf(
+			/* Translators: %1$d is the number of total orders, %2$d is the number of batches. */
+			__( '%1$d orders processed in %2$d batches.', 'wc-custom-order-table' ),
+			$order_count,
+			$batches_processed
+		) );
+	}
 
-        for ($page = $orders_page; $page <= $total_pages; $page++) {
-            $offset = ($page * $orders_batch) - $orders_batch;
-            $sql = $wpdb->prepare($orders_sql . ' LIMIT %d OFFSET %d', $orders_batch, max($offset, 0));
-            $orders = $wpdb->get_col($sql);
+	/**
+	 * Backfill order meta data into postmeta.
+	 *
+	 * ## OPTIONS
+	 *
+	 * [--batch=<batch>]
+	 * : The number of orders to process.
+	 * ---
+	 * default: 1000
+	 * ---
+	 *
+	 * [--page=<page>]
+	 * : The page to start from.
+	 * ---
+	 * default: 1
+	 * ---
+	 *
+	 * ## EXAMPLES
+	 *
+	 *     wp wc-order-table backfill --batch=100 --page=1
+	 *
+	 * @global $wpdb
+	 *
+	 * @param array $args       Positional arguments passed to the command.
+	 * @param array $assoc_args Associative arguments (options) passed to the command.
+	 */
+	public function backfill( $args, $assoc_args ) {
+		global $wpdb;
 
-            foreach ($orders as $order) {
-                // Accessing the order via wc_get_order will automatically migrate the order to the custom table.
-                wc_get_order($order);
+		$orders_batch      = isset( $assoc_args['batch'] ) ? absint( $assoc_args['batch'] ) : 1000;
+		$orders_page       = isset( $assoc_args['page'] ) ? absint( $assoc_args['page'] ) : 1;
+		$order_table       = wc_custom_order_table()->get_table_name();
+		$order_count       = $wpdb->get_var( "SELECT COUNT(1) FROM {$order_table} o" );
+		$total_pages       = ceil( $order_count / $orders_batch );
+		$progress          = \WP_CLI\Utils\make_progress_bar( 'Order Data Migration', $order_count );
+		$batches_processed = 0;
 
-                $progress->tick();
-            }
+		WP_CLI::log( sprintf( __( '%d orders to be backfilled.', 'wc-custom-order-table' ), $order_count ) );
 
-            $batches_processed++;
-        }
+		for ( $page = $orders_page; $page <= $total_pages; $page++ ) {
+			$offset = ( $page * $orders_batch ) - $orders_batch;
+			$orders = $wpdb->get_col( $wpdb->prepare(
+				"SELECT order_id FROM {$order_table} o LIMIT %d OFFSET %d",
+				$orders_batch,
+				max( $offset, 0 )
+			) );
 
-        $progress->finish();
+			foreach ( $orders as $order ) {
+				// Accessing the order via wc_get_order will automatically migrate the order to the custom table.
+				$order = wc_get_order( $order );
+				$order->get_data_store()->backfill_postmeta( $order );
 
-        WP_CLI::log(sprintf(__('%d orders processed in %d batches.', 'wc-custom-order-table'), $order_count, $batches_processed));
-    }
+				$progress->tick();
+			}
 
-    /**
-     * Backfill order meta data into postmeta.
-     *
-     * ## OPTIONS
-     *
-     * [--batch=<batch>]
-     * : The number of orders to process.
-     * ---
-     * default: 1000
-     * ---
-     *
-     * [--page=<page>]
-     * : The page to start from.
-     * ---
-     * default: 1
-     * ---
-     *
-     * ## EXAMPLES
-     *
-     *     wp wc-order-table backfill --batch=100 --page=1
-     *
-     */
-    public function backfill($args, $assoc_args)
-    {
-        global $wpdb;
+			$batches_processed++;
+		}
 
-        $orders_batch = isset($assoc_args['batch']) ? absint($assoc_args['batch']) : 1000;
-        $orders_page = isset($assoc_args['page']) ? absint($assoc_args['page']) : 1;
+		$progress->finish();
 
-        $order_table = wc_custom_order_table()->get_table_name();
-
-        $order_count = $wpdb->get_var("SELECT COUNT(1) FROM {$order_table} o" );
-
-        WP_CLI::log( sprintf( __( '%d orders to be backfilled.', 'wc-custom-order-table' ), $order_count ) );
-
-        $total_pages = ceil($order_count / $orders_batch);
-
-        $progress = \WP_CLI\Utils\make_progress_bar('Order Data Migration', $order_count);
-
-        $orders_sql = "SELECT order_id FROM {$order_table} o";
-        $batches_processed = 0;
-
-        for ($page = $orders_page; $page <= $total_pages; $page++) {
-            $offset = ($page * $orders_batch) - $orders_batch;
-            $sql = $wpdb->prepare($orders_sql . ' LIMIT %d OFFSET %d', $orders_batch, max($offset, 0));
-            $orders = $wpdb->get_col($sql);
-
-            foreach ($orders as $order) {
-                // Accessing the order via wc_get_order will automatically migrate the order to the custom table.
-                $order = wc_get_order($order);
-                $order->get_data_store()->backfill_postmeta( $order );
-
-                $progress->tick();
-            }
-
-            $batches_processed++;
-        }
-
-        $progress->finish();
-
-        WP_CLI::log(sprintf(__('%d orders processed in %d batches.', 'wc-custom-order-table'), $order_count, $batches_processed));
-    }
+		WP_CLI::log( sprintf(
+			/* Translators: %1$d is the number of total orders, %2$d is the number of batches. */
+			__( '%1$d orders processed in %2$d batches.', 'wc-custom-order-table' ),
+			$order_count,
+			$batches_processed
+		) );
+	}
 }
