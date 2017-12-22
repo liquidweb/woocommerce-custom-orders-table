ec0217bb5df28bd441d24e7edaf28e692c8ef683
Steve Grunwell <sgrunwell@liquidweb.com>
2017-12-22 20:49:26 +0000

    Generate plugin tests using WP-CLI's scaffolding

---
+++ tests/test-sample.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Class SampleTest
+ *
+ * @package Woocommerce_Order_Tables
+ */
+
+/**
+ * Sample test case.
+ */
+class SampleTest extends WP_UnitTestCase {
+
+	/**
+	 * A single example test.
+	 */
+	function test_sample() {
+		// Replace this with some actual testing code.
+		$this->assertTrue( true );
+	}
+}
