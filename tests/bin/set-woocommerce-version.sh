33740d652cfd1e7aa336e40281407b446168be82
Steve Grunwell <sgrunwell@liquidweb.com>
2018-01-22 19:01:03 +0000

    Add a mechanism by which we can test against different releases of WooCommerce

---
+++ tests/bin/set-woocommerce-version.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+#
+# Enable the version of WooCommerce used as a Composer dependency to be changed.
+#
+# Usage:
+#
+#     set-woocommerce-version.sh [version]
+
+WC_VERSION=${1-latest}
+
+if [[ $WC_VERSION != 'latest' ]]; then
+	echo "Using WooCommerce version ${WC_VERSION}:"
+	composer require --dev --no-update "woocommerce/woocommerce:${WC_VERSION}"
+else
+	echo "Using WooCommerce at master."
+	composer require --dev --no-update woocommerce/woocommerce:dev-master
+fi
