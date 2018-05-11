445409efb3e35ff26dde21e2398f591b12f5c9c9
Steve Grunwell <sgrunwell@liquidweb.com>
2018-05-11 14:34:15 +0000

    Since the test suite can cause some strange pathing, focus on getting {dir-name}/woocommerce-custom-orders-table.php

--- tests/testcase.php
+++ tests/testcase.php
@@ -124,7 +124,7 @@ class TestCase extends WC_Unit_Test_Case {
 	 * Emulate deactivating, then subsequently reactivating the plugin.
 	 */
 	protected static function reactivate_plugin() {
-		$plugin = plugin_basename( dirname( __DIR__ ) . '/woocommerce-custom-orders-table.php' );
+		$plugin = basename( dirname( __DIR__ ) ) . '/woocommerce-custom-orders-table.php';
 
 		do_action( 'deactivate_' . $plugin, false );
 		do_action( 'activate_' . $plugin, false );
