3bdc01eb625238c5becb0c6689e28a72213be045
Steve Grunwell <sgrunwell@liquidweb.com>
2018-12-14 15:11:01 +0000

    Adjust the logic in tests around duplicate order keys.
    Two tests, `DataStoreTest::test_populate_from_meta_handles_errors()` and `CLITest::test_migrate_skips_on_database_error()`, were explicitly setting order keys to empty strings, which is being fixed in #97. This commit changes the tests to use generic (but still identical) order key values.

--- tests/test-cli.php
+++ tests/test-cli.php
@@ -119,16 +119,16 @@ class CLITest extends TestCase {
 
 		$this->toggle_use_custom_table( false );
 		$order1 = WC_Helper_Order::create_order();
-		$order1->set_order_key( '' );
+		$order1->set_order_key( 'some-key' );
 		$order1->save();
 		$order2 = WC_Helper_Order::create_order();
-		$order2->set_order_key( '' );
+		$order2->set_order_key( 'some-key' );
 		$order2->save();
 		$this->toggle_use_custom_table( true );
 
 		$this->cli->migrate();
 
-		$this->cli->assertReceivedMessageContaining( "Duplicate entry '' for key 'order_key'", 'warning' );
+		$this->cli->assertReceivedMessageContaining( "Duplicate entry 'some-key' for key 'order_key'", 'warning' );
 	}
 
 	public function test_migrate_catches_infinite_loops() {
