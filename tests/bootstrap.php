dfe822a285f2c3ca7b41040ef5bc43f2c2b79311
Steve Grunwell <sgrunwell@liquidweb.com>
2020-02-12 02:11:18 +0000

    Dynamically rewrite the PHPUnit "core" test suite at runtime
    This is some weird, wild stuff, my friends:
    
    The plugin traditionally has two test suites: "plugin" (e.g. just the plugin code), and "core" (running the WooCommerce core test suite with the WooCommerce Custom Orders Table enabled).
    
    The thinking here is "anything that works in WooCommerce without the plugin active should still work once it's turned on."
    
    Since the phpunit.xml(.dist) file doesn't support variables, we have to hijack it with tests/bootstrap.php: once we've determined which version of WooCommerce we're testing against (default is "master"), the bootstrap file will grab the `PHPUnit\Util\Configuration` instance associated with `phpunit.xml.dist` in this repo, find the definition for the `<testsuite name="core" />`, then replace instances of "%WC_VERSION%" with the suffix corresponding to the local copy of WooCommerce (e.g. "3.8" in "woocommerce/woocommerce-src-3.8").
    
    As a bonus, we can now exclude breaking core tests on a per-version basis, which is pretty nifty.

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -9,12 +9,14 @@
  * @author  Liquid Web
  */
 
+use PHPUnit\Util\Configuration;
+
 $_tests_dir = getenv( 'WP_TESTS_DIR' ) ?: rtrim( sys_get_temp_dir(), '/\\' ) . '/wordpress-tests-lib';
 
 // Determine which version of WooCommerce we're testing against.
-$wc_version  = getenv('WC_VERSION') ?: 'latest';
-$target_dir  = dirname( __DIR__ ) . '/vendor/woocommerce/woocommerce-src-';
-$target_dir .= preg_match( '/\d+\.\d+/', $wc_version, $match ) ? $match[0] : 'master';
+$wc_version    = getenv('WC_VERSION') ?: 'latest';
+$target_suffix = preg_match( '/\d+\.\d+/', $wc_version, $match ) ? $match[0] : 'master';
+$target_dir    = dirname( __DIR__ ) . '/vendor/woocommerce/woocommerce-src-' . $target_suffix;
 
 // Attempt to install the given version of WooCommerce if it doesn't already exist.
 if ( ! is_dir( $target_dir ) ) {
@@ -50,6 +52,34 @@ if ( ! file_exists( $_bootstrap ) ) {
 	);
 	exit( 1 );
 }
+
+/*
+ * Since PHPUnit won't let us dynamically define test suites based on $wc_version, use reflection
+ * to inject them into the parsed representation of the XML.
+ */
+$config_xml_path    = dirname( __DIR__ ) . '/phpunit.xml.dist';
+$instances_property = new \ReflectionProperty(Configuration::class, 'instances');
+$instances_property->setAccessible( true );
+$xpath_property     = new \ReflectionProperty(Configuration::class, 'xpath');
+$xpath_property->setAccessible( true );
+
+// Retrieve the parsed config from phpunit.xml.dist.
+$config = $instances_property->getValue();
+$xargs  = $xpath_property->getValue( $config[ $config_xml_path ] );
+
+// Update the *FIRST* <directory> value inline for the "core" test suite.
+$core = $xargs->query('testsuites/testsuite[@name="core"]');
+
+if ( ! empty( $core ) ) {
+	// Replace any %WC_VERSION% placeholders in paths.
+	foreach ( $core->item( 0 )->childNodes as $node ) {
+		$node->textContent = str_replace( '%WC_VERSION%', $target_suffix, $node->textContent );
+	}
+} else {
+	echo 'Unable to override the "core" testsuite within PHPUnit, using default.';
+}
+
+unset( $config_xml_path, $instances_property, $xpath_property, $config, $xargs, $core );
 
 // Finally, Start up the WP testing environment.
 require_once dirname( __DIR__ ) . '/vendor/autoload.php';
